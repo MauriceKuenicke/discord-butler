61cc80d5ef4e93fac90cae56853e86d0560ff8d7
Maurice Künicke <m.kuenicke1995@gmail.com>
2021-05-12 20:16:26 +0200

    first draft for weekly time analysis

--- google_doc/__init__.py
+++ google_doc/__init__.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import numpy as np
 
 
 
@@ -21,12 +22,57 @@ class GoogleDoc:
 
 
 def handle_records(records):
-    cols = {"Dimi\n[WAR]"   : "Dimi", 
+    cols = {"Dimi\n[WAR]"   : "Dimi",  
             "K'shefu\n[GNB]": "K'shefu", 
-            "Minah\n[AST]"  : "Minah", 
+            "Minah\n[AST]"  : "Minah",
+            "Test 1\n[WHM]": "Test 1", 
             "Kikio\n[DNC]"  : "Kikio", 
-            "Mira\n[RDM]"   : "Mira"}
+            "Mira\n[RDM]"   : "Mira",
+            "Test 2 \n[MCH]": "Test 2",
+            "Test 3 \n[SAM]": "Test 3"}
     df = pd.DataFrame(records)
-    df.drop(["Datum", "Zeiten"], axis=1, inplace=True)
+    df.drop(["Zeiten"], axis=1, inplace=True)
     df.rename(columns=cols, inplace=True)
     return df
+
+
+def calculate_available_days(df_copy):
+    df_copy.drop([""], axis=1, inplace=True)
+    start_df = pd.DataFrame()
+    end_df = pd.DataFrame()
+    df_copy.replace(r'^\s*$', "-", regex=True, inplace=True)
+    df_copy.replace(r'x', "-", inplace=True)
+    df_copy.replace(r'xxx', "-", inplace=True)
+    for player in df_copy.columns[2:-1]:
+        start_df.loc[:,player+"_start"] = df_copy[player].apply(lambda x: get_start_time(x))
+        end_df.loc[:,player+"_end"] = df_copy[player].apply(lambda x: get_end_time(x))
+
+    df_copy.loc[:,"start_time"] = start_df.max(axis=1)
+    df_copy.loc[:,"end_time"] = end_df.min(axis=1)
+    df_copy.loc[:,"blocked"] = ~(df_copy != '-').all(1)
+    return df_copy[~df_copy["blocked"]]
+
+
+def get_start_time(x):
+    if x.strip() == "?":
+        return 10
+    elif x.strip() == "" or x.strip() == "-":
+        return np.nan
+    else:
+        start = x.strip().split("-")[0]
+        if start == "?" or start == "":
+            return 10
+        else:
+            return int(start)
+    
+def get_end_time(x):
+    if x.strip() == "?":
+        return 24
+    elif x.strip() == "" or x.strip() == "-":
+        return np.nan
+    else:
+        end = x.strip().split("-")[1]
+        if end == "?" or end == "":
+            return 24
+        else:
+            return int(end)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands, tasks
+import asyncio
 import discord
 import google_doc
+import datetime
 import os
 load_dotenv()
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
@@ -16,10 +18,6 @@ async def on_ready() :
 @client.command()
 async def ping(ctx) :
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
-
-@client.command(name="whoami")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
 
 @client.command()
 async def clear(ctx, amount=20) :
@@ -32,25 +30,75 @@ async def times(ctx, name="availability") :
     gd.authorize_client()
     records = gd.open_by_url(os.getenv("DOC_LINK"))
     df = google_doc.handle_records(records)
+    print(df.head())
     # Filter maybe?
     df = df[:7] 
     # -----------
     data_set = list(df.itertuples(index=False, name=None))
-
-    # Create embeded message for data
     embed = discord.Embed(title=f"__**Zeiten:**__", color=0x03f8fc)
     player = tuple(i for i in df.columns[1:])
     for cnt, day in enumerate(df["Wochentag"]):
-        embed.add_field(name=f'**{day}**', value=f'{player[0]}:  {data_set[cnt][1]}' +
-        f'\n {player[1]}:  {data_set[cnt][2]}' +
+        embed.add_field(name=f'**{day}**', value=f'{player[1]}:  {data_set[cnt][2]}' +
         f'\n {player[2]}:  {data_set[cnt][3]}' +
         f'\n {player[3]}:  {data_set[cnt][4]}' +
         f'\n {player[4]}:  {data_set[cnt][5]}' +
-        f'\n {player[5]}:  {data_set[cnt][6]}\n ', inline=True)
+        f'\n {player[5]}:  {data_set[cnt][6]}' +
+        f'\n {player[6]}:  {data_set[cnt][7]}' +
+        f'\n {player[7]}:  {data_set[cnt][8]}' +
+        f'\n {player[8]}:  {data_set[cnt][9]}\n ', inline=True)
     
     await ctx.send(embed=embed)
-    await ctx.send("\nMich dünkt, es haben einige vergessen ihre Zeiten einzutragen.")
 
 
+@tasks.loop(hours=168.0)
+async def weekly_report():
+    # Get Google document
+    gd = google_doc.GoogleDoc()
+    gd.authorize_client()
+    records = gd.open_by_url(os.getenv("DOC_LINK"))
+    df = google_doc.handle_records(records)
+    df = df[:7]
+    # Calculate available days
+    available_days = google_doc.calculate_available_days(df)
 
+    # Create time table
+    data_set = list(df.itertuples(index=False, name=None))
+    embed = discord.Embed(title=f"__**Zeiten:**__", color=0x03f8fc)
+    player = tuple(i for i in df.columns[1:])
+    for cnt, day in enumerate(df["Wochentag"]):
+        embed.add_field(name=f'**{day}**', value=f'{player[1]}:  {data_set[cnt][2]}' +
+        f'\n {player[2]}:  {data_set[cnt][3]}' +
+        f'\n {player[3]}:  {data_set[cnt][4]}' +
+        f'\n {player[4]}:  {data_set[cnt][5]}' +
+        f'\n {player[5]}:  {data_set[cnt][6]}' +
+        f'\n {player[6]}:  {data_set[cnt][7]}' +
+        f'\n {player[7]}:  {data_set[cnt][8]}' +
+        f'\n {player[8]}:  {data_set[cnt][9]}\n ', inline=True)
+
+    # Create and send message
+    message_string = ""
+    for index, data in available_days.iterrows():
+        message_string += f'{data["Wochentag"]}({data["Datum"]}) von {int(data["start_time"])}Uhr bis {int(data["end_time"])}Uhr\n'
+    print(message_string)
+
+    await client.get_user(356526822906920963).send("Guten Abend Sir! Hier finden Sie die aktuelle Übersicht der eingetragenen Zeiten für die kommende Woche:")
+    await client.get_user(356526822906920963).send(embed=embed)
+    await client.get_user(356526822906920963).send("Ich habe mir die Freiheit genommen und Ihnen bereits die möglichen Raidtage herausgesucht. Aktuell stehen Ihnen folgende Optionen zur Verfügung:\n\n" +
+    message_string)
+
+
+@weekly_report.before_loop
+async def before():
+    await client.wait_until_ready()
+    time_now = datetime.datetime.now()
+    time_goal = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, 12, 19, 52, 0, 0)
+    delt = time_goal - time_now
+    if delt.total_seconds() < 0:
+        time_goal = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day+1, 15, 15, 0, 0)
+        delt = time_goal - time_now
+    print(f"Wait for {delt.total_seconds()} seconds before starting the weekly report.")
+    await asyncio.sleep(delt.total_seconds())
+    print("Finished waiting")
+
+weekly_report.start()
 client.run(token)
