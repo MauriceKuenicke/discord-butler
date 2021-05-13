465b2ec75f6951db92c2afbaa75f065fccd24b40
Maurice Künicke <m.kuenicke1995@gmail.com>
2021-05-13 14:38:47 +0200

    Set weekly report time

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import asyncio
 import discord
 import google_doc
 import utils
-import datetime
 import os
 load_dotenv()
 
@@ -34,8 +33,6 @@ async def times(ctx, name="availability"):
     embed = utils.embeds.createTimeTable(df)
     await ctx.send(f"Aktuell eingetragene Zeiten für die Raidwoche ({df['Datum'].min():%d.%m.%Y}-{df['Datum'].max():%d.%m.%Y})\n")
     await ctx.send(embed=embed)
-    username = client.get_user(356526822906920963)
-    print(username)
 
 
 @tasks.loop(hours=168.0)  # 168 hours = 1 week
@@ -48,18 +45,19 @@ async def send_weekly_report():
     message_string = ""
     for index, data in available_days.iterrows():
         message_string += f'{data["Wochentag"]}({data["Datum"]:%d.%m.%Y}) von {int(data["start_time"])}Uhr bis {int(data["end_time"])}Uhr\n'
-    print(message_string)
 
-    await client.get_user(356526822906920963).send("Guten Abend Sir! Hier finden Sie die aktuelle Übersicht der eingetragenen Zeiten für die kommende Woche:")
-    await client.get_user(356526822906920963).send(embed=embed)
-    await client.get_user(356526822906920963).send("Ich habe mir die Freiheit genommen und Ihnen bereits die möglichen Raidtage herausgesucht. Aktuell stehen folgende Optionen zur Verfügung:\n\n" +
-                                                   message_string)
+    for user_id in [356526822906920963]:
+        username = str(client.get_user(356526822906920963)).split("#")[0]
+        await client.get_user(user_id).send(f"Guten Abend {username}! Hier finden Sie die aktuelle Übersicht der eingetragenen Zeiten für die kommende Woche:")
+        await client.get_user(user_id).send(embed=embed)
+        await client.get_user(user_id).send("Ich habe mir die Freiheit genommen und Ihnen bereits die möglichen Raidtage herausgesucht. Aktuell stehen folgende Optionen zur Verfügung:\n\n" +
+                                            message_string)
 
 
 @send_weekly_report.before_loop
 async def before_weekly():
     await client.wait_until_ready()
-    delta = utils.calculate_timedelta(3, 18, 0)   # Sunday 18:00 -> (6, 18, 0)
+    delta = utils.calculate_timedelta(6, 18, 0)   # Sunday 18:00 -> (6, 18, 0)
     print(f"Starting weekly report cycle in {delta} seconds.")
     await asyncio.sleep(delta)
     print("Starting weekly report cycle.")
